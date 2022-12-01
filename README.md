b1bd16af4a7eb772103cf20c89d65eb07850ffac
Rameez Khan <rameezkhan.sa@gmail.com>
2022-12-01 13:24:30 +0200

    Add code

---
+++ README.md
@@ -0,0 +1,8 @@
+# advent-of-code-2021-python
+
+> My advent of code solutions for 2021 in Python
+
+## Generate the puzzle for the day
+```shell
+make generate
+```
---
+++ common/timing.py
@@ -0,0 +1,17 @@
+from functools import wraps
+import typing
+import time
+
+
+def timeit(f: typing.Callable):
+    """Logs the execution time of a function"""
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = f()
+        t2 = time.time()
+        print(f"Execution time = {(t2 - t1) * 10**3} ms")
+        return result
+
+    return wrapper
